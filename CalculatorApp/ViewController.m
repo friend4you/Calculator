ace64b4d363988e6ea1d45b51079ea8e60f8a3f7
Vladyslav Arseniuk <Vladyslav_Arseniuk@epam.com>
2017-10-30 14:03:43 +0300

    some refactor

--- CalculatorApp/ViewController.m
+++ CalculatorApp/ViewController.m
@@ -14,14 +14,7 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *resultLabel;
 @property (assign, nonatomic) BOOL isUserInTheMiddleOfNumber;
-@property (assign, nonatomic) BOOL isFirstNumberInExpression;
 @property (assign, nonatomic) double displayValue;
-@property (assign, nonatomic) double firstPartOfExpression;
-@property (assign, nonatomic) double secondPartOfExpression;
-@property (assign, nonatomic) BOOL isPlus;
-@property (assign, nonatomic) BOOL isMinus;
-@property (assign, nonatomic) BOOL isMultiple;
-@property (assign, nonatomic) BOOL isDivide;
 
 @end
 
@@ -35,14 +28,9 @@
     self.resultLabel.text = @(displayValue).stringValue;
 }
 
--(void)resetDisplayValue{
-    self.isUserInTheMiddleOfNumber = NO;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.isUserInTheMiddleOfNumber = NO;
-    self.isFirstNumberInExpression = YES;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -53,9 +41,7 @@
 }
 
 - (IBAction)pressDigitButton:(UIButton *)sender {
-    if([self.resultLabel.text length] == 16){
-        return;
-    }
+    if([self.resultLabel.text length] == 16) return;
     if(self.isUserInTheMiddleOfNumber){
         self.resultLabel.text = [self.resultLabel.text stringByAppendingString:sender.titleLabel.text];
     }else{
@@ -65,40 +51,15 @@
 }
 
 - (IBAction)pressOperationButton:(UIButton *)sender {
-    if ([sender.titleLabel.text isEqualToString:@"C"]){
-        [self resetDisplayValue];
-        self.isFirstNumberInExpression = YES;
-    }else if([sender.titleLabel.text isEqualToString:@"√"]){
+    if([sender.titleLabel.text isEqualToString:@"√"]){
         self.displayValue = sqrt(self.displayValue);
-        self.isUserInTheMiddleOfNumber = NO;
     }else if ([sender.titleLabel.text isEqualToString:@"𝞹"]){
         self.displayValue = M_PI;
-        self.isUserInTheMiddleOfNumber = NO;
     }else if ([sender.titleLabel.text isEqualToString:@"x²"]){
         self.displayValue = pow(self.displayValue, 2);
-        self.isUserInTheMiddleOfNumber = NO;
     }else if ([sender.titleLabel.text isEqualToString:@"log₂"]){
         self.displayValue = log2(self.displayValue);
-        self.isUserInTheMiddleOfNumber = NO;
-    }/*else if ([sender.titleLabel.text isEqualToString:@"+"]){
-        self.isPlus = YES;
-        NSLog(@"Before plus %@", @(temp));
-        temp += self.displayValue;
-        [self resetDisplayValue];
-        NSLog(@"After plus %@", @(temp));
-    }else if ([sender.titleLabel.text isEqualToString:@"-"]){
-        temp -= self.displayValue;
-        [self resetDisplayValue];
-    }else if ([sender.titleLabel.text isEqualToString:@"*"]){
-        
-        temp *= self.displayValue;
-        [self resetDisplayValue];
-    }else if ([sender.titleLabel.text isEqualToString:@"/"]){
-        temp /= self.displayValue;
-        [self resetDisplayValue];
-    }else if ([sender.titleLabel.text isEqualToString:@"="]){
-        self.displayValue = temp;
     }
-    self.displayValue = temp;*/
+    self.isUserInTheMiddleOfNumber = NO;
 }
 @end
